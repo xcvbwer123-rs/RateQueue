5d43e2bf8378a32c8c047dd220bab7861abb362f
xcvbwer123-rs <xcvbwer123@gmail.com>
2024-05-31 13:16:16 +0900

    예시/테스트용 스크립트 수정

--- src/test.server.lua
+++ src/test.server.lua
@@ -1,8 +1,36 @@
 ---@module module
 local RateQueue = require(game:GetService("ReplicatedStorage"):WaitForChild("RateQueue"))
 
-local testTask = RateQueue.new(function(a, b, c)
-    print("run begin")
-    print(a, b, c)
-    return "asdf"
-end, "a", "b", "c")
+-- 초당 1번씩만 실행되는 큐
+local TestQueue = RateQueue.new("TestQueue", nil, 1)
+local progress = 0
+
+for index = 1, 10 do
+    TestQueue:insert(function()
+        task.wait(math.random()) -- Yield 하는것도 넣을수 있음
+        print(`Task #{index} Completed.`)
+    end):andThen(function()
+        progress += 1
+        print(`Progress : {progress} / 10`)
+    end)
+end
+
+TestQueue:insertFront(function()
+    print("Front inserted task!")
+end)
+
+local process = TestQueue:insert(function()
+    print("this may ignored!")
+end)
+
+TestQueue:insert(function()
+    print("Last inserted task!")
+end):await():andThen(function()
+    print("all tasks completed!!")
+end)
+
+if TestQueue:remove(process) then
+    print("remove completed!")
+else
+    print("remove failed!")
+end
